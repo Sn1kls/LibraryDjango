d1a6324136e5e6dbd5887358ef9b2b6d61b3b865
Sn1kls <macks.chigrin@gmail.com>
2025-01-22 11:26:01 +0200

    remove files

--- users/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from users.views import UserDetailView, PasswordResetView
-
-urlpatterns = [
-    path('<int:pk>/', UserDetailView.as_view(), name='user-detail'),
-    path('password-reset/', PasswordResetView.as_view(), name='password-reset'),
-]
